6e2d6ab6fd7af72403c70f9d6d0729d525f726a2
naims <naimdrive6@gmail.com>
2025-12-25 13:19:37 +0600

    initialized project

---
+++ lib/dbConnect.ts
@@ -0,0 +1,22 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = process.env.MONGODB_URI
+const dbName = process.env.DBNAME
+
+if (!uri) throw new Error("MONGODB_URI missing");
+if (!dbName) throw new Error("DBNAME missing");
+
+const collestions = {
+    PRODUCTS: "products",
+}
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export const dbConnect=(collectionName: string) => {
+    return client.db(dbName).collection(collectionName)
+}
